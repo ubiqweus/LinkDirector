e213761861837d724abb527f1fde3eb0967d83da
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-06-26 12:18:11 -0400

    Mustache based template

--- Package.swift
+++ Package.swift
@@ -1,18 +1,18 @@
 // swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// Generated automatically by Perfect Assistant
+// Date: 2018-06-20 22:13:14 +0000
 import PackageDescription
 
 let package = Package(
-    name: "LinkDirector",
-    dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "LinkDirector",
-            dependencies: ["PerfectHTTPServer"]),
-    ]
+	name: "LinkDirector",
+	dependencies: [
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"4.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", "3.0.0"..<"4.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Redis.git", "3.0.0"..<"4.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-CloudFormation.git", "0.0.0"..<"1.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", from: "3.0.0")
+	],
+	targets: [
+		.target(name: "LinkDirector", dependencies: ["PerfectHTTPServer", "PerfectPostgreSQL", "PerfectRedis", "PerfectCloudFormation", "PerfectMustache"])
+	]
 )
--- Sources/LinkDirector/main.swift
+++ Sources/LinkDirector/main.swift
@@ -1,7 +1,12 @@
 
 import PerfectHTTPServer
 import PerfectHTTP
+import PerfectMustache
 import Foundation
+
+// ...
+import PerfectPostgreSQL
+import PerfectRedis
 
 #if os(macOS)
 let httpPort = 8080
@@ -9,35 +14,71 @@ let httpPort = 8080
 let httpPort = 80
 #endif
 
+let appStoreURL = "https://itunes.apple.com/app/qbiq/id1183031049"
+let shareTemplate = "./templates/share.mustache"
+
 struct BiqIdRequest: Codable {
 	let biqid: String
 	let token: UUID?
 }
 
-func handleRoot(request: HTTPRequest, response: HTTPResponse) {
-	response.setBody(string: "Ubiqwe.us link director")
-	response.completed()
+extension HTTPResponse {
+	func debug() {
+		var accum = "\n\n--\n\n"
+		let headers = request.headers
+		for (name, value) in headers {
+			accum += "\(name.standardName): \(value)\n"
+		}
+		accum += "\n--\n\n"
+		let queryParams = request.queryParams
+		for (name, value) in queryParams {
+			accum += "\(name)=\(value)\n"
+		}
+		accum += "\n--\n\n"
+		let postParams = request.postParams
+		for (name, value) in postParams {
+			accum += "\(name)=\(value)\n"
+		}
+		accum += "\n--\n\n"
+		print(accum)
+	}
+}
+
+func handleStore(request: HTTPRequest, response: HTTPResponse) {
+	response.setHeader(.location, value: appStoreURL)
+	response.completed(status: .movedPermanently)
+}
+
+func handleRest(request: HTTPRequest, response: HTTPResponse) {
+	StaticFileHandler(documentRoot: "./webroot", allowResponseFilters: true)
+		.handleRequest(request: request, response: response)
 }
 
 func handleTokenBiqURL(request: HTTPRequest, response: HTTPResponse) {
-	guard let request: BiqIdRequest = try? request.decode() else {
-		return response.setBody(string: "Not found").completed(status: .notFound)
+	guard let biqRequest: BiqIdRequest = try? request.decode() else {
+		return handleRest(request: request, response: response)
 	}
-	response.setBody(string: "Ubiqwe.us token/biq request: \(request)")
-	response.completed()
-}
-
-func handleBiqURL(request: HTTPRequest, response: HTTPResponse) {
-	guard let request: BiqIdRequest = try? request.decode() else {
-		return response.setBody(string: "Not found").completed(status: .notFound)
+	let qBiqUrl: String
+	if let token = biqRequest.token {
+		qBiqUrl = "qbiq://qbiq/\(token)/\(biqRequest.biqid)"
+	} else {
+		qBiqUrl = "qbiq://qbiq/\(biqRequest.biqid)"
 	}
-	response.setBody(string: "Ubiqwe.us biq request: \(request)")
-	response.completed()
+	let map: [String:Any] = ["appStoreURL":appStoreURL, "qBiqUrl":qBiqUrl]
+	let ctx = MustacheEvaluationContext(templatePath: shareTemplate, map: map)
+	let collector = MustacheEvaluationOutputCollector()
+	do {
+		let txt = try ctx.formulateResponse(withCollector: collector)
+		response.setBody(string: txt).completed(status: .found)
+	} catch {
+		response.setBody(string: "\(error)").completed(status: .internalServerError)
+	}
 }
 
 var routes = Routes()
-routes.add(method: .get, uri: "/", handler: handleRoot)
-routes.add(method: .get, uri: "/{biqid}", handler: handleBiqURL)
+routes.add(method: .get, uri: "/**", handler: handleRest)
+routes.add(method: .get, uri: "/", handler: handleStore)
+routes.add(method: .get, uri: "/{biqid}", handler: handleTokenBiqURL)
 routes.add(method: .get, uri: "/{token}/{biqid}", handler: handleTokenBiqURL)
 
 try HTTPServer.launch(name: "ubiqwe.us", port: httpPort, routes: routes)
